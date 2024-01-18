5274d339dfc7ce8d7dfa40207748c38e27196596
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-01-18 10:54:33 -0500

    commit

--- 02_01_Generics/Assignment/src/App.java
+++ 02_01_Generics/Assignment/src/App.java
@@ -7,5 +7,22 @@ public class App {
         myStringProperty.set("Hello");
         System.out.println(myStringProperty.get());
 
+        Property<Integer> myInteger = new Property<Integer>();
+        myInteger.set(3);
+        System.out.println(myInteger.get());
+        myInteger.set(2);
+        System.out.println(myInteger.get());
+
+        Property<Long> myLong = new Property<Long>();
+        myLong.set(349877389L);
+        System.out.println(myLong.get());
+        myLong.set(905665901L);
+        System.out.println(myLong.get());
+
+        Property<Boolean> myBoo = new Property<Boolean>();
+        myBoo.set(true);
+        System.out.println(myBoo.get());
+        myBoo.set(false);
+        System.out.println(myBoo.get());
     }
 }
