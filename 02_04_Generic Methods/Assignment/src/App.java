aac0f3a39f4051607e084d4ce1072798c018fde5
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-02-02 10:26:15 -0500

    commit

--- 02_04_Generic Methods/Assignment/src/App.java
+++ 02_04_Generic Methods/Assignment/src/App.java
@@ -3,12 +3,12 @@ public class App {
         Property<Double> number1 = new Property<Double>(3.0);
         Property<Double> number2 = new Property<Double>(2.0);
 
-        System.out.println("number1 > number2" + number1.isGreaterThan(number2));
-        System.out.println("number1 > number2" + number1.isLessThan(number2));
-        System.out.println("number1 > number2" + number1.equals(number2));
-        System.out.println("number1 > number2" + number1.isnotequals(number2));
-        System.out.println("number1 > number2" + number1.greaterThanorequals(number2));
-        System.out.println("number1 > number2" + number1.isLessThanOrEqual(number2));
+        System.out.println("number1 + number2" + number1.isGreaterThan(number2));
+        System.out.println("number1 - number2" + number1.isLessThan(number2));
+        System.out.println("number1 = number2" + number1.equals(number2));
+        System.out.println("number1 /= number2" + number1.isNotEqual(number2));
+        System.out.println("number1 < number2" + number1.isGreaterThan(number2));
+        System.out.println("number1 => number2" + number1.isLessThanOrEqual(number2));
         //TODO:  repeat for equals, notequals, greaterThanorequals, less than or equals
 
     }
