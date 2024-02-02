7a39188a37dae214338d96c8e9a96adcbc8032ad
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-02-02 10:46:04 -0500

    commit

--- 03_File Handling/02_Reading a File/Assignment/src/App.java
+++ 03_File Handling/02_Reading a File/Assignment/src/App.java
@@ -1,5 +1,18 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileName = "src/ender.txt";
+
+        Path filePath = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(filePath);
+
+        for(var line : lines){
+            System.out.println(line);
+        }
+        
     }
 }
