9aae9fff59028aad0547193266d5576c00975e71
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-02-02 10:58:55 -0500

    commit

--- 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
+++ 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
@@ -1,17 +1,18 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
-        String fileName = "ender.txt";
+        String fileName = "src/ender.txt";
 
         Path filePath = Paths.get(fileName);
         List<String> lines = Files.readAllLines(filePath);
+
+        for(var line : lines){
+            System.out.println(line);
+        }
+        
     }
 }
