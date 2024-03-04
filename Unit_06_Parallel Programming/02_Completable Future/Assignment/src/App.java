0447978c054cc5091aa7e03d29f0628d2679380a
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-03-04 10:43:55 -0500

    commit

--- Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
+++ Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
@@ -1,5 +1,18 @@
+import java.util.function.BiFunction;
+import java.util.concurrent.CompletableFuture;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        BiFunction<Integer, Integer, Integer> randoGenerator = (min, max) -> {
+            int size = max - min;
+            double rando = size * Math.random();
+            return (int) Math.floor(rando);
+        };
+
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> randoGenerator.apply(0, 10));
+
+        System.out.println(future.get());
     }
+
 }
