5817da774d030df22423adca7be3a768f7ecc264
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-03-08 10:45:43 -0500

    commit

--- 04_Functional Programming_Streams/05_Iterating Streams/Assignment/src/App.java
+++ 04_Functional Programming_Streams/05_Iterating Streams/Assignment/src/App.java
@@ -9,23 +9,23 @@ public class App {
         // use streams to generate a list of numbers from 0 to 10
         // use streams and Functions with lambdas to make a list with 5 added to each numbers in the ints list
         // use streams and foreach to print out the values
-        Stream<String> myStringStream = Stream.of(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0);
-        myStringStream.stream().
-        Stream<Function> numbers = new Stream<Function>();
-            numbers.add(5);
-
+        List<Double> myStringStream = Stream.of(0.0 ,1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0).toList();
+        Function<Double, Double> add5 = (number) -> number + 5;
+        List<Double> myDoubleAdded5 = myStringStream.stream().map(add5).toList();
+        System.out.println(myStringStream);
+        System.out.println(myDoubleAdded5);
         //What else you need to do.  
         //TODO:  repeate the mapping wtih an extra div2Function and map the list of intsPlus5 to make
         // a list intsPlus5Div2.  
         //TODO:  use foreach to print intsPlus5Div2
 
-        List<Integer> ints = IntStream.rangeClosed(0, 10).boxed().toList();
-        Function<Integer, Integer> add5Func = (number) -> number;  //TODO:  acutally implement the lambda correctly
 
-        List<Integer> intsPlus5 = ints.stream().map(add5Func).toList();
+        List<Integer> myIntStream = IntStream.rangeClosed(0, 10).boxed().toList();
+        Function<Integer, Integer> div2Func = (number) -> number / 2;  //TODO:  acutally implement the lambda correctly
+        List<Integer> intsPlus5 = myIntStream.stream().map(div2Func).toList();
+        System.out.println(myIntStream);
+        System.out.println(intsPlus5);
 
-        Consumer<Integer> intPrinter = (number) -> System.out.println(number);
-        intsPlus5.stream().forEach(intPrinter);
 
         //TODO:  write a div2Func Function like I did with add5Func
         //TODO:  apply div2Func to intsPlus5 with map
